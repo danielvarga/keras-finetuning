659351c24f1d47d36fa7c04b19219ce72e02c0e7
Daniel Varga <daniel.varga@prezi.com>
2016-09-10 02:17:03 +0200

    works for small collection, fails for big one.

--- collect-apple-photos.sh
+++ collect-apple-photos.sh
@@ -1,3 +1,13 @@
-sqlite3 -separator $'\t' ImageProxies.apdb "select modelId, resourceUuid, filename from RKModelResource order by modelId;" | awk 'BEGIN{FS="\t"; for(n=0;n<256;n++)ord[sprintf("%c",n)]=n}  { print $1 "\t" ord[substr($2,1,1)] "/" ord[substr($2,2,1)] "/" $2 "/" $3 }' > filenames
-sqlite3 -separator $'\t' Person.db "select f.modelId,f.personId,p.name from RKFace f join RKPerson p on f.personId=p.modelId order by f.modelId;" > faces
+#!/bin/bash
 
+if [ -z "$1" ] || [ -z "$2" ] ; then echo "Usage: $0 PATH_TO_APPLE_PHOTOS_WORKING_DIR OUTPUT_DIR"; exit -1 ; fi
+
+PHOTOSPATH=$1
+OUTPUT_DIR=$2
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sqlite3 -separator $'\t' $1/database/ImageProxies.apdb "select modelId, resourceUuid, filename from RKModelResource order by modelId;" | awk 'BEGIN{FS="\t"; for(n=0;n<256;n++)ord[sprintf("%c",n)]=n}  { print $1 "\t" ord[substr($2,1,1)] "/" ord[substr($2,2,1)] "/" $2 "/" $3 }' > imagefiles
+sqlite3 -separator $'\t' $1/database/Person.db "select f.modelId,f.personId,p.name from RKFace f join RKPerson p on f.personId=p.modelId order by f.modelId;" > faces
+
+python $DIR/collect_apple_photos.py imagefiles faces $PHOTOSPATH/resources/modelresources $OUTPUT_DIR
--- collect_apple_photos.py
+++ collect_apple_photos.py
@@ -1,16 +1,28 @@
 import sys
 import os
 import shutil
+import unicodedata
 from collections import defaultdict
+
+
+UNKNOWN = "unknown"
+
+def remove_accents(input_unicode):
+    nfkd_form = unicodedata.normalize('NFKD', input_unicode)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+def normalized(p):
+    p = p.replace("/", "-")
+    p = p.replace(" ", "-")
+    return remove_accents(p.decode("utf-8")).encode("ascii")
+
 
 def main():
     files_filename, faces_filename, input_dir, output_dir = sys.argv[1:]
     files = [l.strip().split("\t") for l in file(files_filename)]
     faces = [l.strip().split("\t") for l in file(faces_filename)]
     persons = set(l[2] for l in faces)
-    assert "unknown" not in persons
-    print "list of people:"
-    print "\n".join(sorted(persons))
+    assert UNKNOWN not in persons
     print len(files), "files found"
     files = [(indx, filename) for (indx, filename) in files if filename.lower().endswith((".jpg", ".png"))]
     print len(files), "image files kept"
@@ -20,21 +32,27 @@ def main():
     print len(common), "images with face information"
     d = defaultdict(list)
     for (indx, filename) in files:
-        person = faces_dict.get(indx, "unknown")
+        person = faces_dict.get(indx, UNKNOWN)
         d[person].append(filename)
+    if UNKNOWN in d.keys():
+        persons.add(UNKNOWN)
+    print
+    print "images found for each person:"
     for p in sorted(persons):
         print p, len(d[p])
 
-    def normalized(p):
-        p = p.replace("/", "-")
-        p = p.replace(" ", "-")
-        return p
-
-    os.mkdir(output_dir)
+    try:
+        os.mkdir(output_dir)
+    except:
+        print "please remove the output directory"
+        raise
     for p in persons:
         os.mkdir(os.path.join(output_dir, normalized(p)))
     for p in persons:
         for filename in d[p]:
-            shutil.copy(os.path.join(input_dir, filename), os.path.join(output_dir, normalized(p), filename))
+            c1, c2, uu, f = filename.split("/")
+            generated_filename = uu+"-"+f
+            shutil.copy(os.path.join(input_dir, filename), os.path.join(output_dir, normalized(p), generated_filename))
+
 
 main()
